be672d829257e8158a7ebcc42967938fa50b78ae
valle314 <valentinbart47@googlemail.com>
2023-05-07 13:47:38 +0200

    change back to grub

--- hyprland-setup-nvidia.sh
+++ hyprland-setup-nvidia.sh
@@ -44,9 +44,8 @@ sudo sed -i 's/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/
 sudo mkinitcpio --config /etc/mkinitcpio.conf --generate /boot/initramfs-custom.img
 echo "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf
 
-#TODO ändere das zu systemd!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet nvidia_drm.modeset=1"/' /etc/default/grub
-# sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet nvidia_drm.modeset=1"/' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 #disable wifi powersafe mode
 LOC="/etc/NetworkManager/conf.d/wifi-powersave.conf"
